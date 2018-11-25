ce85e8d074b2224d00ea85b319071c947f734aab
Dervis Mehmed Barutcu <43811839+DervisMehmed@users.noreply.github.com>
2018-11-26 00:53:47 +0300

    Add files via upload

--- src/FirstIterationCodes/Piece10.java
+++ src/FirstIterationCodes/Piece10.java
@@ -11,49 +11,51 @@ import javafx.scene.shape.Shape;
  *
  * @author Dervis
  */
-public class Piece8 extends  Pieces {
+public class Piece10 extends  Pieces {
     
     Circle arrayOfCircles[];
     Shape shape3;
     public Shape createPiece() {
         
-        int circleNumberX = 3;
+        int circleNumberX = 2;
         int circleNumberY = 1;
-        int subCircleNo = circleNumberX + circleNumberY;
-        int circleNumber = circleNumberX + circleNumberY + 1;
+        int circleNumber2X = 2;
+        
+        int subCircleNo1 = circleNumberX + circleNumberY;
+        int circleNumber = circleNumberX + circleNumberY + circleNumber2X;
+        
         arrayOfCircles = new Circle[circleNumber] ;
         int radius = 50 ;
         int diameter = radius + radius  ;
         int distance = diameter ;
+        
         //array e ilk atılan sol alt olmalıdır
         //Yan yana cirlelar için SADECE
-        for(int i = 0; i < circleNumberX; i++)
+        for(int i=0;i<circleNumberX;i++)
         { 
-            arrayOfCircles[i]=createCircle(radius,350.0f + distance,680.0f);
-            distance = distance + diameter; 
-            System.out.println("x"+i);
+            arrayOfCircles[i]=createCircle(radius,750.0f + distance,680.0f);
+            distance = distance + diameter;
         }
         distance = diameter;
 
-        for(int i=circleNumberX; i < subCircleNo; i++)
+        for(int i=circleNumberX;i<subCircleNo1;i++)
         {
-            arrayOfCircles[i]=createCircle(radius, 550.0f, 680.0f - distance);
+            arrayOfCircles[i]=createCircle(radius,750.0f + distance, 680.0f + distance);
             distance = distance + diameter ;
-            System.out.println("y"+i);
         }
         distance = diameter;
-          
-        for(int i = subCircleNo; i < circleNumber; i++)
-        { 
-            arrayOfCircles[i]=createCircle(radius,550.0f + distance, 680.0f + distance);
-            distance = distance + diameter; 
-            System.out.println("x"+i);
+
+        for(int i=subCircleNo1;i<circleNumber;i++)
+        {
+            arrayOfCircles[i]=createCircle(radius,850.0f + distance,580.0f);
+            distance = distance + diameter ;
         }
           
         shape3 = createUnion(arrayOfCircles);
-        shape3.setFill(Color.ORANGERED);
+        shape3.setFill(Color.VIOLET);
         handlerELİF ( shape3, arrayOfCircles );
         return shape3;
+     
     }
     public void detectIntersection( Rectangle rec1, double[][] arx, double[][] ary, int row, int col, boolean[][] arf  ){
         handlerZeynepHande ( shape3, arrayOfCircles, rec1, arx, ary, row, col, arf ); 
--- src/FirstIterationCodes/Piece9.java
+++ src/FirstIterationCodes/Piece9.java
@@ -1,6 +1,5 @@
 package ıqpuzzlerpro;
 
-
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -11,16 +10,16 @@ import javafx.scene.shape.Shape;
  *
  * @author Dervis
  */
-public class Piece8 extends  Pieces {
+public class Piece9 extends  Pieces {
     
     Circle arrayOfCircles[];
     Shape shape3;
     public Shape createPiece() {
         
-        int circleNumberX = 3;
+        int circleNumberX = 2;
         int circleNumberY = 1;
         int subCircleNo = circleNumberX + circleNumberY;
-        int circleNumber = circleNumberX + circleNumberY + 1;
+        int circleNumber = circleNumberX + circleNumberY + 2;
         arrayOfCircles = new Circle[circleNumber] ;
         int radius = 50 ;
         int diameter = radius + radius  ;
@@ -29,7 +28,7 @@ public class Piece8 extends  Pieces {
         //Yan yana cirlelar için SADECE
         for(int i = 0; i < circleNumberX; i++)
         { 
-            arrayOfCircles[i]=createCircle(radius,350.0f + distance,680.0f);
+            arrayOfCircles[i]=createCircle(radius,350.0f + distance,350.0f);
             distance = distance + diameter; 
             System.out.println("x"+i);
         }
@@ -37,25 +36,26 @@ public class Piece8 extends  Pieces {
 
         for(int i=circleNumberX; i < subCircleNo; i++)
         {
-            arrayOfCircles[i]=createCircle(radius, 550.0f, 680.0f - distance);
+            arrayOfCircles[i]=createCircle(radius, 550.0f, 350.0f - distance);
             distance = distance + diameter ;
             System.out.println("y"+i);
         }
+          
         distance = diameter;
           
         for(int i = subCircleNo; i < circleNumber; i++)
         { 
-            arrayOfCircles[i]=createCircle(radius,550.0f + distance, 680.0f + distance);
+            arrayOfCircles[i]=createCircle(radius,550.0f + distance, 250.0f);
             distance = distance + diameter; 
             System.out.println("x"+i);
         }
           
         shape3 = createUnion(arrayOfCircles);
-        shape3.setFill(Color.ORANGERED);
-        handlerELİF ( shape3, arrayOfCircles );
+        shape3.setFill(Color.PURPLE);
+        //handlerELİF ( shape3, arrayOfCircles );
         return shape3;
     }
     public void detectIntersection( Rectangle rec1, double[][] arx, double[][] ary, int row, int col, boolean[][] arf  ){
-        handlerZeynepHande ( shape3, arrayOfCircles, rec1, arx, ary, row, col, arf ); 
+        //handlerZeynepHande ( shape3, arrayOfCircles, rec1, arx, ary, row, col, arf ); 
     }
 }
